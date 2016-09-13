4d10587f0782b843eda986dbb804ebc1c5dfb760
Markku Laine <markku.laine@aalto.fi>
2016-09-13 17:35:21 +0300

    Published version 0.0.2
    Todo: clean and optimize code, add filters & UI eye candy

---
+++ js/boulder_2016.js
@@ -0,0 +1,104 @@
+var dataSource;
+var map;
+var data;
+var HIGHLY_RECOMMENDED_LIMIT  = 6;
+
+
+// Hide sidebar
+document.getElementById( 'sidebar-close' ).addEventListener( 'click', function () {
+  document.getElementById( 'sidebar' ).style.display  = 'none';
+});
+
+// Initialize Application
+function initialize() {
+  // Load data from a remote CSV file
+  dataSource      = new DataSource( 'data/boulder_2016.csv', initializeMap );
+}
+
+// Initialize Google Maps
+function initializeMap() {
+  // Get loaded data
+  data            = dataSource.getData();
+
+  // Initialize the map
+  map             = new google.maps.Map( document.getElementById( 'map' ), {
+    center: { lat: 65.1939658, lng: 26.6251252 }, // Finland
+    zoom: 5
+  });
+
+  var oms         = new OverlappingMarkerSpiderfier( map, { markersWontMove: true, markersWontHide: true, keepSpiderfied: true } );
+  var bounds      = new google.maps.LatLngBounds();
+
+  // Place markers on the map
+  var marker;
+  var iconCluster = 'img/icon_boulder_cluster.png';
+  var iconBasic   = 'img/icon_boulder_quality.png';
+  var iconTop     = 'img/icon_boulder_top_quality.png';
+  var infowindow  = new google.maps.InfoWindow();
+  // Global OMS lisneter
+  oms.addListener( 'click', function( marker, event ) {
+    infowindow.setContent( marker.content );
+    infowindow.open( map, marker );
+  });
+
+  var content;
+  var markers     = [];
+  for ( var i = 0; i < data.length; i++ ) {
+    marker        = new google.maps.Marker({
+      position: new google.maps.LatLng( data[ i ].Latitude, data[ i ].Longitude ),
+      map: map,
+      icon: ( data[ i ].Votes >= HIGHLY_RECOMMENDED_LIMIT ) ? iconTop : iconBasic
+    });
+    marker.content   = getInfoWindowContent( data[ i ] );
+    oms.addMarker( marker );
+    bounds.extend( marker.getPosition() );
+    markers.push( marker );
+  }
+
+  // Create a marker cluster
+  var markerCluster = new MarkerClusterer( map, markers, { maxZoom: 10, styles: [ { url: iconCluster, height: 40, width: 40, anchor: [ 40, 40 ], textColor: '#000000', textSize: 12 } ] } );
+
+  // Zoom in/out so that all markers are visible
+  map.fitBounds( bounds );
+}
+
+function getInfoWindowContent( data ) {
+  var content       = '';
+  var name          = data.Name;
+  var grade         = ( data.Grade !== '' ) ? data.Grade : '?';
+  var boulder       = name + ' (' + grade + ')';
+  var crag          = ( data.Crag !== '' ) ? data.Crag : '';
+  var area          = ( data.AreaLevel3 !== '' ) ? data.AreaLevel3 : ( data.AreaLevel2 !== '' ) ? data.AreaLevel2 : data.AreaLevel1;
+  area              = ( data.ApproximateLocation === 'Yes' ) ? '<span class="approximate-location">Tuntematon sijainti</span>' : area;
+  var location      = ( crag !== '' ) ? crag + ', ' + area : area;
+  var linksArray    = [];
+  if ( data.Url27crags !== '' ) {
+    linksArray.push( '<a href="' + data.Url27crags + '" target="_blank">27crags</a>' );
+  }
+  if ( data.ApproximateLocation === 'No' ) {
+    linksArray.push( '<a href="https://maps.google.com/maps?saddr=Current+Location&daddr=' + data.Latitude + '%2C%20' + data.Longitude + '" target="_blank">Reittiohjeet</a>' );
+  }
+  if ( data.UrlVideo !== '' ) {
+    linksArray.push( '<a href="' + data.UrlVideo + '" target="_blank">Video</a>' );
+  }
+  if ( data.UrlStory !== '' ) {
+    linksArray.push( '<a href="' + data.UrlStory + '" target="_blank">Tarina</a>' );
+  }
+
+  var links         = '';
+  for ( j = 0; j < linksArray.length; j++ ) {
+    links           += linksArray[ j ];
+    if ( j < ( linksArray.length - 1 ) ) {
+      links         += '&nbsp;&middot;&nbsp;';
+    }
+  }
+
+  content           =
+    '<div class="marker-info">' +
+    '<h3>' + boulder + '</h3>' +
+    '<p>' + location + '</p>' +
+    '<p>' + links + '</p>' +
+    '</div>';
+
+  return content;
+}
