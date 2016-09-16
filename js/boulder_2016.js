6b71f2ff1e802fca015d1253c25b352cabd52171
Markku Laine <markku.laine@aalto.fi>
2016-09-16 23:23:55 +0300

    Filters work individually

--- js/boulder_2016.js
+++ js/boulder_2016.js
@@ -6,9 +6,8 @@ var dataSource;
 var infoWindow;
 var map;
 var markerClusterer;
-var markers;
 var MIN_VOTES_FOR_TOP_QUALITY = 6;
-var GRADES                    = [ '?', '1', '2', '3', '4', '4+', '5', '5+', '6A', '6A+', '6B', '6B+', '6C', '6C+', '7A', '7A+', '7B', '7B+', '7C', '7C+', '8A' ];
+var GRADES                    = [ '?', '1', '2', '3', '4', '4+', '5', '5+', '6A', '6A+', '6B', '6B+', '6C', '6C+', '7A', '7A+', '7B', '7B+', '7C', '7C+', '8A', '8A+', '8B', '8B+', '8C', '8C+', '9A' ];
 
 
 
@@ -18,7 +17,7 @@ var GRADES                    = [ '?', '1', '2', '3', '4', '4+', '5', '5+', '6A'
 // Initialize Application
 function initialize() {
   // Load data
-  dataSource           = new DataSource({
+  dataSource         = new DataSource({
     callback: initializeMap,
     encoding: 'utf-8',
     file: 'data/boulder_2016.csv',
@@ -29,20 +28,20 @@ function initialize() {
 // Initialize Google Maps
 function initializeMap() {
   // Create a map
-  map                  = createMap( document.getElementById( 'map' ) );
+  map                = createMap( document.getElementById( 'map' ) );
 
   // Get boulders
-  boulders             = dataSource.getData();
+  boulders           = dataSource.getData();
 
   // Marker icons
-  var icons            = {
+  var icons          = {
     basic:   'img/marker_boulder_basic_quality.png',
     top:     'img/marker_boulder_top_quality.png',
     cluster: 'img/marker_boulder_cluster.png'
   };
 
   // Create an information window for markers
-  infoWindow           = new google.maps.InfoWindow();
+  infoWindow         = new google.maps.InfoWindow();
 
   // Create an overlapping marker spiderfier
   var markerSpiderfier = createOverlappingMarkerSpiderfier();
@@ -53,51 +52,44 @@ function initializeMap() {
   });
 
   // Create a bounds area object for markers
-  var bounds           = new google.maps.LatLngBounds();
+  var bounds         = new google.maps.LatLngBounds();
 
-  // Hold markers in quality groups
-  markers              = {};
+  // Hold markers
+  var markers        = [];
 
   // Create markers
   for ( var i = 0; i < boulders.length; i++ ) {
-    var boulder   = boulders[ i ];
-    var quality   = ( boulder.Votes >= MIN_VOTES_FOR_TOP_QUALITY ) ? 'top' : 'basic';
-    var content   = getInfoWindowContent( boulder );
-    var gradeNumber = boulder.GradeSortable;
-    var icon      = icons[ quality ];
-    var position  = new google.maps.LatLng( boulder.Latitude, boulder.Longitude );
-    var zIndex    = ( quality === 'top' ) ? 2 : 1;
+    var boulder      = boulders[ i ];
+    var quality      = ( boulder.Votes >= MIN_VOTES_FOR_TOP_QUALITY ) ? 'top' : 'basic';
+    var content      = getInfoWindowContent( boulder );
+    var gradeNumeric = boulder.GradeNumeric;
+    var icon         = icons[ quality ];
+    var position     = new google.maps.LatLng( boulder.Latitude, boulder.Longitude );
+    var zIndex       = ( quality === 'top' ) ? 2 : 1;
     
-    // Create an array for each quality group
-    if ( quality in markers === false ) {
-      markers[ quality ] = [];
-    }
-
     // Create a marker
-    var marker         = createMarker( content, gradeNumber, {
+    var marker       = createMarker( content, quality, gradeNumeric, {
       icon:     icon,
       position: position,
       zIndex:   zIndex
     });
 
-    // Add marker to be part of the marker spiderfier
-    markerSpiderfier.addMarker( marker );
-
     // Extend bounds with the marker
     bounds.extend( marker.getPosition() );
 
-    // Assign the marker into a dedicated quality group
-    markers[ quality ].push( marker );
+    // Add the marker into the array
+    markers.push( marker );
+
+    // Add marker to be part of the marker spiderfier
+    markerSpiderfier.addMarker( marker );
   }
 
   // Create a marker clusterer
-  markerClusterer      = createMarkerClusterer( {
-    icon: icons.cluster
+  markerClusterer    = createMarkerClusterer( markers, {
+    icon: icons.cluster,
+    ignoreHidden: true
   });
 
-  // Add all markers to the map
-  markerClusterer.addMarkers( markers[ 'basic' ] );
-  markerClusterer.addMarkers( markers[ 'top' ] );
 
   // Zoom map in/out to fit bounds
   map.fitBounds( bounds );
@@ -114,8 +106,9 @@ function createMap( id ) {
   });
 }
 
-function createMarkerClusterer( options ) {
-  return new MarkerClusterer( map, [], {
+function createMarkerClusterer( markers, options ) {
+  return new MarkerClusterer( map, markers, {
+    ignoreHidden: options.ignoreHidden,
     maxZoom: 10,
     minimumClusterSize: 3,
     styles: [
@@ -134,20 +127,21 @@ function createMarkerClusterer( options ) {
 function createOverlappingMarkerSpiderfier() {
   return new OverlappingMarkerSpiderfier( map, {
     markersWontMove: true,
-    markersWontHide: true,
+    //markersWontHide: true,
     keepSpiderfied: true
   });
 }
 
-function createMarker( content, gradeNumber, options ) {
-  var marker     = new google.maps.Marker({
+function createMarker( content, quality, gradeNumeric, options ) {
+  var marker          = new google.maps.Marker({
     icon: options.icon,
     position: options.position,
     zIndex: options.zIndex
   });
-  // Set marker content for the marker spiderfier
-  marker.content = content;
-  marker.gradeNumber = gradeNumber;
+  // Set extra properties on the marker
+  marker.content      = content;
+  marker.quality      = quality;
+  marker.gradeNumeric = gradeNumeric;
 
   return marker;
 }
@@ -198,9 +192,8 @@ function getInfoWindowContent( data ) {
 /////////////////////////////////////////////////////////////////////////////
 // JQUERY FUNCTIONS
 /////////////////////////////////////////////////////////////////////////////
-$( '#ex2' ).slider({
+$( '#grade-filter' ).slider({
   tooltip: 'always',
-  step: 1,
   formatter: function( range ) {
     if ( range[ 0 ] === range[ 1 ] ) {
       return GRADES[ range[ 0 ] ];
@@ -211,64 +204,76 @@ $( '#ex2' ).slider({
   }
 });
 
-$( '#ex2' ).on( 'slideStop', function( event ) {
-}
-
-$( '#ex2' ).on( 'change', function( event ) {
-  var oldValueMin = event.value.oldValue[ 0 ];
-  var oldValueMax = event.value.oldValue[ 1 ];
-  var newValueMin = event.value.newValue[ 0 ];
-  var newValueMax = event.value.newValue[ 1 ];
-
-  if ( oldValueMin !== newValueMin || oldValueMax !== newValueMax ) {
-    markerClusterer.clearMarkers();
-    // Iterate over basic quality boulders
-    for ( var i = 0; i < markers[ 'basic' ].length; i++ ) {
-      var marker = markers[ 'basic' ][ i ];
-      if ( newValueMin <= marker.gradeNumber && marker.gradeNumber <= newValueMax ) {
-        markerClusterer.addMarker( marker );
-      }
-    }
-
-    if ( newValueMin === newValueMax ) {
-      $( '#slaideriarvo' ).text( GRADES[ newValueMax ] );
-    }
-    else {
-      $( '#slaideriarvo' ).text( GRADES[ newValueMin ] + ' - ' + GRADES[ newValueMax ] );
-    }
-  }
-});
-
-//#slaideriarvo
 
 /////////////////////////////////////////////////////////////////////////////
 // EVENT LISTENERS
 /////////////////////////////////////////////////////////////////////////////
-// Add filter listeners
-document.getElementById( 'top-quality-filter' ).addEventListener( 'change', function () {
-  if ( this.checked === true ) {
-    // Add top quality markers to the map
-    markerClusterer.addMarkers( markers[ 'top' ] );
-  }
-  else {
-    // Remove top quality markers from the map
-    for ( var i = 0; i < markers[ 'top' ].length; i++ ) {
-      markerClusterer.removeMarker( markers[ 'top' ][ i ] );
+$( document ).ready( function() {
+  initialize();
+});
+
+$( '#grade-filter' ).on( 'change', function( event ) {
+  var oldRange    = event.value.oldValue;
+  var newRange    = event.value.newValue;
+
+  if ( oldRange[ 0 ] !== newRange[ 0 ] || oldRange[ 1 ] !== newRange[ 1 ] ) {
+    var markers = markerClusterer.getMarkers();
+
+    for ( var i = 0; i < markers.length; i++ ) {
+      var marker = markers[ i ];
+
+      if ( newRange[ 0 ] <= marker.gradeNumeric && marker.gradeNumeric <= newRange[ 1 ] ) {
+        marker.setVisible( true );
+      }
+      else {
+        marker.setVisible( false );        
+      }
+    }
+    markerClusterer.repaint();
+
+    if ( newRange[ 0 ] === newRange[ 1 ] ) {
+      $( '#grade-display' ).text( GRADES[ newRange[ 1 ] ] );
+    }
+    else {
+      $( '#grade-display' ).text( GRADES[ newRange[ 0 ] ] + ' - ' + GRADES[ newRange[ 1 ] ] );
     }
   }
 });
 
-document.getElementById( 'basic-quality-filter' ).addEventListener( 'change', function () {
-  if ( this.checked === true ) {
-    // Add basic quality markers to the map
-    markerClusterer.addMarkers( markers[ 'basic' ] );
-  }
-  else {
-    // Remove basic quality markers from the map
-    for ( var i = 0; i < markers[ 'basic' ].length; i++ ) {
-      markerClusterer.removeMarker( markers[ 'basic' ][ i ] );
+$( '#top-quality-filter' ).on( 'change', function () {
+  var markers = markerClusterer.getMarkers();
+  
+  for ( var i = 0; i < markers.length; i++ ) {
+    var marker = markers[ i ];
+
+    if ( marker.quality === 'top' ) {
+      if ( this.checked === true ) {
+        marker.setVisible( true );
+      }
+      else {
+        marker.setVisible( false );        
+      }
     }
   }
+  markerClusterer.repaint();
+});
+
+$( '#basic-quality-filter' ).on( 'change', function () {
+  var markers = markerClusterer.getMarkers();
+  
+  for ( var i = 0; i < markers.length; i++ ) {
+    var marker = markers[ i ];
+
+    if ( marker.quality === 'basic' ) {
+      if ( this.checked === true ) {
+        marker.setVisible( true );
+      }
+      else {
+        marker.setVisible( false );        
+      }
+    }
+  }
+  markerClusterer.repaint();
 });
 
 document.getElementById( 'sidebar-close' ).addEventListener( 'click', function () {
