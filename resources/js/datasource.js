76994a8a61b6a334f5c834e6092a01542b5ee030
Markku Laine <markku.laine@aalto.fi>
2016-09-28 19:19:53 +0300

    Added new layout and filters

---
+++ resources/js/datasource.js
@@ -0,0 +1,22 @@
+// Constructors
+var DataSource  = function( options ) {
+  this.data     = [];
+  this.loaded   = options.callback;
+
+  // Load CSV data
+  var that      = this;
+  Papa.parse( options.file, {
+    complete: function( results ) {
+      that.data = results.data;
+      that.loaded();
+    },
+    download: true,
+    encoding: options.encoding,
+    header: options.header
+  });
+};
+
+// Methods
+DataSource.prototype.getData  = function() {
+  return this.data;
+};
